e5c5973101086b7624d3655042b994a4bc09c281
kierwl <45115106+kierwl@users.noreply.github.com>
2025-01-21 21:03:40 +0900

    Update

---
+++ Assets/Scripts/LodadScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    static LoadScene instance;
+
+    public static void LoadScene 
+}
