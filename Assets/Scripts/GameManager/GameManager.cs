4673059cdeb62c311507078c76274ffdac701300
chajungto <50168549+chajungto@users.noreply.github.com>
2025-01-22 17:58:15 +0900

    성공 패널 / 실패 패널 / 스크립터블 오브젝트 / EndPanel 스크립트 / GameManager 추가 및 수정 /
    몇몇 버튼들은 스크립트를 아직 추가하지 않아 작동하지 않을 수 있습니다

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;     //싱글톤
     public Text timeTxt;                    //시간 텍스트
-    public GameObject endTxt;               //끝날 때 나오는 텍스트
+    public GameObject endTxt;               //끝날 때 나오는 텍스트 (바뀜)
     public GameObject detailBox;            //카드 설명 패널
     public GameObject emptyPanel;           //카드 터치 방지용
     float time = 0f;                        //시작 시간(현재 시간)
@@ -14,10 +15,17 @@ public class GameManager : MonoBehaviour
     public Card firstCard;                  //첫번째 카드
     public Card secondCard;                 //두번째 카드
     AudioSource audioSource;                //BGM
-    public AudioClip clip;
+    public AudioClip clip;                  //BGM 클립
     public Image detailBoxImg;              //맞춘 카드의 이미지
     public bool isRunning = true;           //시간이 작동하는지
-    
+    public Text ownerTxt;                   //맞춘 카드의 주인
+    public Text detailTxt;                  //맞춘 카드의 TMI
+    public GameObject endPanel;             //성공시의 패널
+    public GameObject failPanel;            //실패시의 패널
+    public List<TMI_Details> tMI_Details;   //TMI 정보들
+
+
+
     void Awake()
     {
         if(Instance == null)
@@ -34,22 +42,30 @@ public class GameManager : MonoBehaviour
         time += Time.deltaTime;             //시간 경과중
         timeTxt.text = time.ToString("N2");
 
-        //60초를 넘거나 카드개수가 0이면서 작동중일 때 게임 그만
-        if((time >= 60.0f) || (cardCount == 0 && isRunning))    
+        if(time >= 60.0f)
         {
-            GameFinish();       
+            GameFail();
+        }
+        if (cardCount == 0 && isRunning)
+        {
+            GameFinish();
         }
     }
     void GameFinish()
     {
         Time.timeScale = 0.0f;              //게임 잠시 멈추고
-        endTxt.SetActive(true);             //끝났다는 글자 등장 -> 이부분을 TMI 나오는 방향으로
+        endPanel.SetActive(true);           //끝났다는 글자 등장 -> 이부분을 TMI 나오는 방향으로
+    }
+    void GameFail()
+    {
+        Time.timeScale = 0.0f;              //게임 잠시 멈추고
+        failPanel.SetActive(true);          //실패 패널 등장
     }
     public void Matched()                   //열어 본 카드 두개 비교
     {
         if(firstCard.cardIdx == secondCard.cardIdx)     //일치할 경우
         {
-            audioSource.PlayOneShot(clip);
+            //audioSource.PlayOneShot(clip);
 
             firstCard.CallDestroyCard();
             secondCard.CallDestroyCard();
@@ -58,7 +74,7 @@ public class GameManager : MonoBehaviour
 
             cardCount -= 2;                             //카드 개수 업데이트
         }
-        else                                            //불일치할 경우
+        else                                            //불일치할 경우 다시 닫기
         {
             firstCard.CallCloseCard();
             secondCard.CallCloseCard();
@@ -74,6 +90,9 @@ public class GameManager : MonoBehaviour
 
         detailBoxImg.sprite = card.frontGO.GetComponent<SpriteRenderer>().sprite;
 
+        int index = card.cardIdx;
+        ownerTxt.text = tMI_Details[index].Owner;
+        detailTxt.text = tMI_Details[index].Detail;
         isRunning = false;
 
         StartCoroutine(ResumeGame());
