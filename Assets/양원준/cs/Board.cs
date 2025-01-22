5fd4f84f2a36bccf7f054b77703516700ffef8de
kierwl <45115106+kierwl@users.noreply.github.com>
2025-01-22 21:18:17 +0900

    보드탓이었네 ;;;
    로드 이름충돌나서 튕겨요

--- Assets/양원준/cs/Board.cs
+++ Assets/양원준/cs/Board.cs
@@ -2,27 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+
 public class Board : MonoBehaviour
 {
     public Transform cards; // 카드의 위치 
     public GameObject card; // 프리팹 카드 오브젝트
     public int cardcount = 0; // 카드의 총 개수 초기값 0
 
-   
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 ,7, 7, 8, 8, 9, 9 }; // 0부터 9 까지 총 10개의 배열이 두개씩 입력됨
-		arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray(); // 0부터 7까지 8개의 배열을 랜덤한 위치에 배정
-        for (int i = 0; i < 20; i++) // 위치의 인덱스값을 섞는 for문 총 16개의 위치를 랜덤하게 지정하기 위함
-        {
-            GameObject go = Instantiate(card, this.transform); // 게임오브젝트의 위치값을 받아옴
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 }; // 0부터 9까지 총 20개의 값
+        arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray(); // 배열을 랜덤한 위치에 배정
 
-            float x = (i % 4) * 1.2f - 2f; // x축 카드의 간격 계산
-            float y = (i / 5) * 1.1f - 3.2f; // y축 카드의 간격 계산
-            
-            go.transform.position = new Vector2(x, y); // 위치값 평면기준으로 벡터2d 생성
-            go.GetComponent<Card>().CardImageSet(arr[i]);// 카드 컴포넌트를 불러오고 세팅함수에서 배열[인덱스번호]로 변경
+        for (int i = 0; i < 20; i++) // 총 20개의 카드를 생성
+        {
+            GameObject go = Instantiate(card, this.transform); // 카드 프리팹 생성
+
+            float x = (i % 4) * 1.2f - 2f; // x축 카드 간격 계산
+            float y = (i / 4) * 1.1f - 3.2f; // y축 카드 간격 계산 (4열 기준)
+
+            go.transform.position = new Vector2(x, y); // 위치값 평면 기준으로 설정
+            go.GetComponent<Card>().CardImageSet(arr[i]); // 카드 이미지 세팅
         }
-        GameManager.Instance.cardCount = arr.Length; // 게임매니저에서 카드의 수를 배열의 길이만큼 변경
+
+        GameManager.Instance.cardCount = arr.Length; // 카드 수를 GameManager에 전달
     }
 }
