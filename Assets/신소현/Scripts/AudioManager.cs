f3cafb3ba033ebe2e5ad3a782426b3db52445fcf
S-SoHyun <slj506@naver.com>
2025-01-23 15:33:27 +0900

    사운드 추가 & 피치 up
    1. 괜찮아 보이는 사운드 몇 개 추가 (안 쓸 수 있음)
    2. AudioManager 스크립트에 50초부터 bgm 피치 높이는 코드 작성해봤는데 혹시 몰라서 주석처리 해놨습니다ㅎ...

--- Assets/신소현/Scripts/AudioManager.cs
+++ Assets/신소현/Scripts/AudioManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance;
+    public static AudioManager Instance; // 싱글톤
 
     AudioSource audioSource;
-    public AudioClip clip;
+    public AudioClip bgm; // bgm
+
 
     private void Awake()
     {
@@ -26,7 +27,20 @@ public class AudioManager : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
 
-        audioSource.clip = this.clip;
-        audioSource.Play();
+        audioSource.clip = bgm; 
+        audioSource.Play(); // bgm 실행
     }
+
+    //private void Update()
+    //// 50, 55초 될 때 사운드 피치 올려서 긴박감 연출 (GamaeManager.cs에서 time 변수 public으로 바꾸는 게 필요함)
+    //{
+    //    if (GameManager.Instance.time > 50.0f)
+    //    {
+    //        audioSource.pitch = 1.2f;
+    //    }
+    //    if (GameManager.Instance.time > 55.0f && GameManager.Instance.time <= 60.0f)
+    //    {
+    //        audioSource.pitch = 1.4f;
+    //    }
+    //}
 }
